b4d80585acb4bf8c5b69fac10c812ab4dfb3b425
Administrator <Administrator@MSDN-SPECIAL>
2018-02-08 16:27:54 +0900

    [김기성] 아나운서 래퍼 클래스 추가 및 index수정 (중간과정)

---
+++ team27a/src/service/Anaunseo.java
@@ -0,0 +1,26 @@
+package service;
+
+public class Anaunseo {
+	private int AnaunseoId;
+	private String AnaunseoName;
+	private int AnaunseoAge;
+	public int getAnaunseoId() {
+		return AnaunseoId;
+	}
+	public void setAnaunseoId(int anaunseoId) {
+		AnaunseoId = anaunseoId;
+	}
+	public String getAnaunseoName() {
+		return AnaunseoName;
+	}
+	public void setAnaunseoName(String anaunseoName) {
+		AnaunseoName = anaunseoName;
+	}
+	public int getAnaunseoAge() {
+		return AnaunseoAge;
+	}
+	public void setAnaunseoAge(int anaunseoAge) {
+		AnaunseoAge = anaunseoAge;
+	}
+	
+}
