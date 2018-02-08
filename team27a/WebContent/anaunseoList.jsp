5cd92dcfd91747256a8f1e21037ba4f65406581e
Administrator <Administrator@MSDN-SPECIAL>
2018-02-08 17:19:49 +0900

    [김기성] 아나운서DAO, List 중간과정

--- team27a/WebContent/anaunseoList.jsp
+++ team27a/WebContent/anaunseoList.jsp
@@ -1,24 +1,18 @@
 <%@ page language="java" contentType="text/html; charset=EUC-KR" pageEncoding="EUC-KR"%>
+<%@ page import = "service.AnaunseoDao" %>
+<%@ page import = "service.Anaunseo" %>
+<%@ page import = "java.util.ArrayList" %>
 <!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
-<title>Insert title here</title>
-</head>
-<body>
-	<%@ page import = "java.sql.DriverManager" %>
-	<%@ page import = "java.sql.Connection" %>
-	<%@ page import = "java.sql.ResultSet" %>
-	<%
-		Connection conn = null;
-		ResultSet rs = null;
-		
-		Class.forName("com.mysql.jdbc.Driver"); //드라이버 로딩
-		String jdbcDriver = "jdbc:mysql://localhost:3306/jjdev?" + "useUnicode=true&characterEncoding=euckr";
-		String dbUser = "root";
-		String dbPass = "java0000";
-		conn = DriverManager.getConnection(jdbcDriver, dbUser, dbPass); //db 연결
-		
-	%>
-</body>
-</html>
+<%
+	request.setCharacterEncoding("euc-kr");
+	AnaunseoDao anaunseoDao = new AnaunseoDao();
+	Anaunseo anaunseo = new Anaunseo();
+	ArrayList<Anaunseo> array = new ArrayList<Anaunseo>();
+%>
+<table>
+	<tr>
+		<td>id</td>
+		<td>name</td>
+		<td>age</td>
+	</tr>
+</table>
--- team27a/src/service/AnaunseoDao.java
+++ team27a/src/service/AnaunseoDao.java
@@ -1,19 +1,49 @@
 package service;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class AnaunseoDao {
-	public Anaunseo selectAnaunseoList() throws SQLException {
-		PreparedStatement pstmt = null;
-		Connection conn = null;
-		ResultSet rs = null;
+	public ArrayList<Anaunseo> selectAnaunseoList() {
+		ArrayList<Anaunseo> array = new ArrayList<Anaunseo>();
+		Connection connection = null;
+		ResultSet resultSet = null;
+		PreparedStatement preparedStatement = null;
 		
-		pstmt = conn.prepareStatement("select * from tb_member");
-		rs = pstmt.executeQuery();
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");  //드라이버 로딩
+			
+			String jdbcDriver = "jdbc:mysql://localhost:3306/jjdev?" + "useUnicode=true&characterEncoding=euckr";
+			String dbUser = "root";
+			String dbPass = "java0000";
+			
+			connection = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
+			
+			preparedStatement = connection.prepareStatement("select * from tb_member");
+			resultSet = preparedStatement.executeQuery(); //쿼리 실행 준비 및 실행
+			
+			while(resultSet.next()) { //쿼리에 내용 있으면 배열에 대입
+				Anaunseo anaunseo = new Anaunseo();
+				anaunseo.setAnaunseoId(resultSet.getInt("AnaunseoId"));
+				anaunseo.setAnaunseoName(resultSet.getString("AnaunseoName"));
+				anaunseo.setAnaunseoAge(resultSet.getInt("AnaunseoAge"));
+				array.add(anaunseo);
+			}
+		}catch (ClassNotFoundException e) { //예외 처리
+			e.printStackTrace();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			if (resultSet != null) try { resultSet.close(); } catch(SQLException ex) {}
+			if (preparedStatement != null) try { preparedStatement.close(); } catch(SQLException ex) {}
+			if (connection != null) try { connection.close(); } catch(SQLException ex) {}
+		}
 		
-		return null;
+		return array; //배열 리턴
 	}
 }
