83603ba965d4556b03ebf8e744cad90333c260bf
smart-202-18 <smart-202-18@smart-202-18-PC>
2018-02-08 17:40:31 +0900

    [김도희]Actor.java추가 ActorDao.java추가 actorList.jsp추가

--- team27a/WebContent/actorList.jsp
+++ team27a/WebContent/actorList.jsp
@@ -1,11 +1,23 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR"pageEncoding="EUC-KR"%>
+<%@ page language="java" contentType="text/html; charset=EUC-KR" pageEncoding="EUC-KR"%>
 <!DOCTYPE html>
+<%@ page import = "service.Actor" %>
+<%@ page import = "service.ActorDao" %>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
 <title>Insert title here</title>
 </head>
 <body>
+<%
+ActorDao actordao = new ActorDao();
+Actor actor =actordao.selectActorList();
+
+String Id=actor.getActorId();
+String Name=actor.getActorName();
+String Age=actor.getActorAge();
+
+%>
+
 
 
 </body>
--- team27a/src/service/ActorDao.java
+++ team27a/src/service/ActorDao.java
@@ -1,26 +1,66 @@
 package service;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+
 
 public class ActorDao {
-	public Connection selectActorList(){
-		Connection recon = null;
+	public static void main(String[] args) {
+		
+		ActorDao a = new ActorDao();
+		a.selectActorList();
+	}
+	
+	public Actor selectActorList(){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		Actor actor = new Actor();
+		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String jdbcDriver = "jdbc:mysql://localhost:3306/jjdevdb?" +
-				"useUnicode=true&characterEncoding=euckr";
+		Class.forName("com.mysql.jdbc.Driver");
+	
+		
+		String Driver = "jdbc:mysql://localhost:3306/jjdev?useUnicode=true&characterEncoding=euckr";
 		String dbUser = "root";
 		String dbPass = "java0000";
-		try {
-			recon = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+		
+		
+		conn = DriverManager.getConnection(Driver, dbUser, dbPass);
+		pstmt = conn.prepareStatement("select * from actor");
+		rs = pstmt.executeQuery();
+		
+		
+		
+		while(rs.next()){
+			
+			String actorId=rs.getString("actor_id");
+			String actorName=rs.getString("actor_name");
+			String actorAge=rs.getString("actor_age");
+			System.out.println(actorId+"<--actorId");
+			System.out.println(actorName+"<--actorName");
+			System.out.println(actorAge+"<--actorAge");
+			
+			actor.setActorId(actorId);
+			actor.setActorName(actorName);
+			actor.setActorAge(actorAge);
+		}
+		
+		} catch(ClassNotFoundException e) {
 			e.printStackTrace();
-		} return recon;
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) try { rs.close(); } catch(SQLException ex) {}
+			if (pstmt != null) try { pstmt.close(); } catch(SQLException ex) {}
+			if (conn != null) try { conn.close(); } catch(SQLException ex) {}
+		}
+		 return actor;
 	}
+		
+
 }
