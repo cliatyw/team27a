de0fcbaf10dc31d0b96abb94dbb62d9c5553fd0e
smart-202-18 <smart-202-18@smart-202-18-PC>
2018-02-08 16:23:16 +0900

    [김도희] Actor.java내용추가 ActorDao.java내용추가

--- team27a/src/service/Actor.java
+++ team27a/src/service/Actor.java
@@ -2,4 +2,30 @@ package service;
 
 public class Actor {
 
+	private String actorId;
+	private String actorName;
+	private String actorAge;
+	public String getActorId() {
+		return actorId;
+	}
+	public void setActorId(String actorId) {
+		this.actorId = actorId;
+	}
+	public String getActorName() {
+		return actorName;
+	}
+	public void setActorName(String actorName) {
+		this.actorName = actorName;
+	}
+	public String getActorAge() {
+		return actorAge;
+	}
+	public void setActorAge(String actorAge) {
+		this.actorAge = actorAge;
+	}
+	
+	
+	
+	
+	
 }
--- team27a/src/service/ActorDao.java
+++ team27a/src/service/ActorDao.java
@@ -1,7 +1,26 @@
 package service;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ActorDao {
-	public static void selectActorList(){
-		
+	public Connection selectActorList(){
+		Connection recon = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String jdbcDriver = "jdbc:mysql://localhost:3306/jjdevdb?" +
+				"useUnicode=true&characterEncoding=euckr";
+		String dbUser = "root";
+		String dbPass = "java0000";
+		try {
+			recon = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} return recon;
 	}
 }
