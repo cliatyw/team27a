4b18dc759673889f78004d679fdac75896172270
Administrator <Administrator@kyungsu>
2018-02-08 16:05:09 +0900

    [최상욱] singer db연결,리스트 추가.

--- team27a/src/service/SingerDao.java
+++ team27a/src/service/SingerDao.java
@@ -1,7 +1,21 @@
 package service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SingerDao {
-	public static void selectActorList(){
-		
+	public Connection driverDbcon() throws ClassNotFoundException, SQLException {
+		Connection conn = null;
+		Class.forName("com.mysql.jdbc.Driver");
+
+		String jdbcDriver = "jdbc:mysql://localhost:3306/jjdev?" + "useUnicode=true&characterEncoding=euckr";
+		String dbUser = "root";
+		String dbPass = "java0000";
+
+		conn = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
+		return conn;
+
 	}
+
 }
