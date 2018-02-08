dffa3ee00506705d04ec8be0973fb48e3ee913f8
백지훈 <@192.168.123.101>
2018-02-08 16:23:13 +0900

    [백지훈] actress db연결, actressList 추가

--- team27a/src/service/Actress.java
+++ team27a/src/service/Actress.java
@@ -1,5 +1,28 @@
 package service;
 
 public class Actress {
+	private String actressId;
+	private String actressName;
+	private String actressAge;
+	
+	public String getActressId() {
+		return actressId;
+	}
+	public void setActressId(String actressId) {
+		this.actressId = actressId;
+	}
+	public String getActressName() {
+		return actressName;
+	}
+	public void setActressName(String actressName) {
+		this.actressName = actressName;
+	}
+	public String getActressAge() {
+		return actressAge;
+	}
+	public void setActressAge(String actressAge) {
+		this.actressAge = actressAge;
+	}
 
+	
 }
